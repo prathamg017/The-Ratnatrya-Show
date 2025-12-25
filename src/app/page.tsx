aa2a049b0bff38c84f6ae5fd72466ee7d29dedc8
apple <apple@apples-MacBook-Air.local>
2025-12-25 14:44:20 +0530

    video update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -387,7 +387,7 @@ export default function Home() {
                   playsInline
                   controls
                 >
-                  <source src="/whatis.MP4" type="video/mp4" />
+                  <source src="/introratnatray.mp4" type="video/mp4" />
                   Your browser does not support the video tag.
                 </video>
               </div>
